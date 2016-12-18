c360ab078da3837cdca8a8643ab75918d178b09b
Geoff Liddiard <geoff.liddiard@gmail.com>
2016-12-18 10:09:31 +0000

    injecting metadata

--- src/app/book/book.config.js
+++ src/app/book/book.config.js
@@ -21,7 +21,14 @@
                         template: '<book-view book="$resolve.book"></book-view><book-suggestions books="$resolve.recommended"></book-suggestions>',
                         resolve: {
                             book: function ($route, BookService){
-                                return BookService.get($route.current.params.bid);
+                                return BookService.get($route.current.params.bid)
+                                    .then(function (book){
+                                        $route.current.metadata = {
+                                            title: book.name + ' | Reedsy',
+                                            description: book.description 
+                                        };
+                                        return book;
+                                    });
                             },
                             recommended: function ($route, BookService){
                                 return BookService.recommend($route.current.params.bid);
